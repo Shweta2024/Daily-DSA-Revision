d0854c659a110bd583d1f5267602a99848553758
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2023-08-14 23:16:52 +0530

    Time: 15 ms (95.23%), Space: 25 MB (25.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,39 +2,43 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
-        int res = 1;
+        int n = nums.size();
+        int totalProduct = 1;
         int zeroCount = 0;
         int zeroIndex = 0;
-        for(int index = 0; index < nums.size(); index++)
+        vector<int>prefix(n,1);
+        vector<int>sufix(n,1);
+        vector<int>ans(n,0);
+        
+        //o(n)
+        for(int index = 0; index < n; index++)
         {
-            int current = nums[index];
-            
-            if(zeroCount > 1)
-                break;
-            
-            if(current == 0)
+            if(nums[index] == 0)
             {
-                zeroIndex = index;
                 zeroCount++;
-            }
-            
-            if(current == 0 && zeroCount == 1)
+                zeroIndex = index;
                 continue;
+            }
+            totalProduct = (long long)totalProduct * nums[index];
             
-            res *= (long long)current;
+            if(index == 0)
+                continue;
+                
+            prefix[index] = (long long)prefix[index-1]*nums[index-1];
+            sufix[n-index-1] = (long long)sufix[n-index]*nums[n-index];
         }
-        vector<int>ans(nums.size(),0);
+        
         if(zeroCount > 1)
             return ans;
         
         if(zeroCount == 1)
         {
-            ans[zeroIndex] = res;
+            ans[zeroIndex] = totalProduct;
             return ans;
         }
-        for(int index = 0; index < nums.size(); index++)
-            ans[index] = res/nums[index];
-      
+        
+        for(int index = 0; index < n; index++)
+            ans[index] = prefix[index] * sufix[index];
         return ans;
     }
 };
