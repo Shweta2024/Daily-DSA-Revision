65114f004c604b6ec937b510ce740917dd9ded27
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-01-05 10:17:09 +0530

    Time: 273 ms (38.47%), Space: 10.9 MB (46.64%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    
-    int lis(int currentIndex, int prev, vector<int>& nums, vector<vector<int>>&memo)
-    {
-        if(currentIndex >= nums.size()) return 0;
-        
-        if(memo[currentIndex][prev+1] != -1) return memo[currentIndex][prev+1];
-        
-        int take = 0;
-        if(prev == -1 || nums[prev] < nums[currentIndex])
-             take = 1 + lis(currentIndex+1,currentIndex,nums,memo);
-        int notTake = lis(currentIndex+1,prev,nums,memo);
-        
-        memo[currentIndex][prev+1] = max(take,notTake);
-        return memo[currentIndex][prev+1];
-    }
-    
     int lengthOfLIS(vector<int>& nums) 
     {
-        vector<vector<int>>memo(nums.size(),vector<int>(nums.size()+1,-1));
-        return lis(0,-1,nums,memo);
+        int maxLen = 1;
+        vector<int>dp(nums.size(),1);
+        
+        for(int currentIndex = 1; currentIndex < nums.size(); currentIndex++)
+        {
+            for(int prevIndex = 0; prevIndex < currentIndex; prevIndex++)
+            {
+                if(nums[prevIndex] < nums[currentIndex])
+                {
+                    dp[currentIndex] = max(dp[prevIndex]+1,dp[currentIndex]);
+                    maxLen = max(maxLen,dp[currentIndex]);
+                }
+            }
+        }
+        return maxLen;
     }
 };
