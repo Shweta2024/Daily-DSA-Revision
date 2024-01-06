5153381061c535b77bdf10955696e2df788aad70
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-01-06 21:25:30 +0530

    Time: 204 ms (39.97%), Space: 84.6 MB (25.19%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1235-maximum-profit-in-job-scheduling.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     
-    int maxProfit(int currentIndex, vector<vector<int>>&jobs, vector<int>&start, vector<int>&memo)
+    int maxProfit(int currentIndex, vector<vector<int>>&jobs, vector<int>&startTime, vector<int>&memo)
     {
         if(currentIndex == jobs.size()) return 0;
         
         if(memo[currentIndex] != -1) return memo[currentIndex];
         
-        int nextIndex = lower_bound(start.begin(),start.end(),jobs[currentIndex][1]) - start.begin();
+        int nextIndex = lower_bound(startTime.begin(),startTime.end(),jobs[currentIndex][1]) - startTime.begin();
         
-        //take
-        int take = jobs[currentIndex][2] + maxProfit(nextIndex,jobs,start,memo);
+        // take
+        int take = jobs[currentIndex][2] + maxProfit(nextIndex,jobs,startTime,memo);
         
-        //not take
-        int notTake = maxProfit(currentIndex+1,jobs,start,memo);
+        // not take
+        int notTake = maxProfit(currentIndex+1,jobs,startTime,memo);
         
         memo[currentIndex] = max(take,notTake);
         return memo[currentIndex];
     }
     
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) 
     {
-        vector<int>memo(startTime.size(),-1);
+        int n = startTime.size();
         vector<vector<int>>jobs;
-        for(int index = 0; index < startTime.size(); index++)
+        vector<int>memo(n,-1);
+        
+        for(int index = 0; index < n; index++)
             jobs.push_back({startTime[index],endTime[index],profit[index]});
         
         sort(jobs.begin(),jobs.end());
         sort(startTime.begin(),startTime.end());
         
-        return maxProfit(0,jobs,startTime,memo); 
+        return maxProfit(0,jobs,startTime,memo);
     }
 };
