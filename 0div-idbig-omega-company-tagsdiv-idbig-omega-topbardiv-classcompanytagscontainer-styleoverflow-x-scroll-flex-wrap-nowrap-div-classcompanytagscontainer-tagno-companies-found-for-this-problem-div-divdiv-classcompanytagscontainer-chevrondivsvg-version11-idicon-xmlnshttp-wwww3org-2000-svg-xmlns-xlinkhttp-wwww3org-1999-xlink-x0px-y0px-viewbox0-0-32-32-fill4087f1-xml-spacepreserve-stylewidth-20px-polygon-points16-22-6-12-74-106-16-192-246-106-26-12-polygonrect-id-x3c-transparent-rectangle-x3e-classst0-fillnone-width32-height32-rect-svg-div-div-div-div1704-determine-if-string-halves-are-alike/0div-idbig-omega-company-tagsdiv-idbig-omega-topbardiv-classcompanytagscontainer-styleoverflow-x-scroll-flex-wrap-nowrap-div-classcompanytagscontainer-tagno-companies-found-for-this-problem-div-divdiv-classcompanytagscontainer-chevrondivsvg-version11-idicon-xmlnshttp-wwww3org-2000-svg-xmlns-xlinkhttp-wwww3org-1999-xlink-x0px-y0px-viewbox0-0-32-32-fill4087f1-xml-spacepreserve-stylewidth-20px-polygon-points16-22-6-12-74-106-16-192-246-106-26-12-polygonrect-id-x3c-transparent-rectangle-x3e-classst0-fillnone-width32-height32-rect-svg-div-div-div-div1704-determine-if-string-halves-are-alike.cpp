006216b4ce097fe1695a89ea869bb81f2ce22bf7
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-01-12 11:33:45 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (72.68%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.cpp
@@ -3,8 +3,8 @@ public:
     
     bool isVowel(char current)
     {
-        if(current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u' || 
-          current == 'A' || current == 'E' || current == 'I' || current == 'O' || current == 'U'  )
+        if(current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u' ||
+          current == 'A' || current == 'E' || current == 'I' || current == 'O' || current == 'U')
             return true;
         return false;
     }
@@ -12,19 +12,33 @@ public:
     bool halvesAreAlike(string s) 
     {
         int n = s.size();
-        int vowelsLeftHalf = 0;
-        int vowelsRightHalf = 0;
-        for(int index = 0; index < n/2; index++)
+        int vowelLeftHalf = 0;
+        int vowelRightHalf = 0;
+        
+        for(int index = 0; index  < n/2; index++)
         {
             // for left half
             if(isVowel(s[index]))
-                vowelsLeftHalf++;
-            
+                vowelLeftHalf++;
             
             // for right half
             if(isVowel(s[n/2+index]))
-                vowelsRightHalf++;
+                vowelRightHalf++;
         }
-        return vowelsLeftHalf == vowelsRightHalf;
+        return vowelLeftHalf == vowelRightHalf;
     }
 };
+
+/*
+
+L   E   E   T     |    C    O   D   E
+0   1   2   3          4    5   6   7
+
+
+2 E E   2 O E
+
+vowelLeftHalf = 0
+vowelRightHalf = 0
+Condition to be alike: they sould have equal no. of vowels
+
+*/
