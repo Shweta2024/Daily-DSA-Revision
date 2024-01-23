2458980456ed78bbdcf95e258c404042073a4d85
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-01-23 10:10:43 +0530

    Time: 96 ms (45.40%), Space: 34.2 MB (57.81%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1239-maximum-length-of-a-concatenated-string-with-unique-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    
+    bool isValid(string &currentString, unordered_map<char,int>&memo)
+    {
+        unordered_map<char,int>temp;
+        for(auto currentChar : currentString)
+        {
+            temp[currentChar]++;
+            if(memo[currentChar] == 1 || temp[currentChar] > 1)
+                return false;
+        }
+        // cout<<"string: "<<currentString<<endl;
+        // for(auto c : temp)
+        //     cout<<c.first<<" : "<<c.second<<endl;
+        // cout<<"*****";
+        return true;
+    }
+    
+    void findMaxLen(int currentIndex, string &current, int &ans, unordered_map<char,int>&memo, vector<string>&arr)
+    {
+        if(currentIndex >= arr.size())
+        {
+            // cout<<current<<endl;
+            int len = current.size();
+            ans = max(ans,len);
+            return;
+        }
+        
+        for(int index = currentIndex; index < arr.size(); index++)
+        {
+            // cout<<currentIndex<<endl;
+            string currentString = arr[index];
+            if(isValid(currentString,memo))
+            {
+                // cout<<currentString<<endl;
+                int i = 0;
+                while(i < currentString.size())
+                {
+                    current.push_back(currentString[i]);
+                    memo[currentString[i]]++;
+                    i++;
+                }
+                cout<<current<<endl;
+                findMaxLen(index+1,current,ans,memo,arr);
+                
+                i = currentString.size();
+                while(i--)
+                {
+                    current.pop_back();
+                    memo[currentString[i]]--;
+                }
+            }
+        }
+        int len = current.size();
+        ans = max(ans,len);
+        return;
+    }
+    
+    int maxLength(vector<string>& arr) 
+    {
+        int ans = 0;
+        string current = "";
+        unordered_map<char,int>memo;
+        
+        findMaxLen(0,current,ans,memo,arr);
+        
+        return ans;
+    }
+};
