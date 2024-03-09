da40ebe35f040e40916bdc3b1ad988972bfce525
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-03-09 11:57:54 +0530

    Time: 137 ms (12.92%), Space: 77 MB (8.70%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) 
+    int getCommon(vector<int>& nums1, vector<int>& nums2)
     {
-        int i = 0;
-        int j = 0;
-        while(i < nums1.size() && j < nums2.size())
+        unordered_map<int,int>memo;
+        for(auto current : nums1)
+            memo[current]++;
+        
+        for(auto current : nums2)
         {
-            if(nums1[i] == nums2[j])
-                return nums1[i];
-            
-            else if(nums1[i] < nums2[j])
-                i++;
-            
-            else // nums1[i] > nums2[j]
-                j++;
+            if(memo[current])
+                return current;
         }
         return -1;
     }
