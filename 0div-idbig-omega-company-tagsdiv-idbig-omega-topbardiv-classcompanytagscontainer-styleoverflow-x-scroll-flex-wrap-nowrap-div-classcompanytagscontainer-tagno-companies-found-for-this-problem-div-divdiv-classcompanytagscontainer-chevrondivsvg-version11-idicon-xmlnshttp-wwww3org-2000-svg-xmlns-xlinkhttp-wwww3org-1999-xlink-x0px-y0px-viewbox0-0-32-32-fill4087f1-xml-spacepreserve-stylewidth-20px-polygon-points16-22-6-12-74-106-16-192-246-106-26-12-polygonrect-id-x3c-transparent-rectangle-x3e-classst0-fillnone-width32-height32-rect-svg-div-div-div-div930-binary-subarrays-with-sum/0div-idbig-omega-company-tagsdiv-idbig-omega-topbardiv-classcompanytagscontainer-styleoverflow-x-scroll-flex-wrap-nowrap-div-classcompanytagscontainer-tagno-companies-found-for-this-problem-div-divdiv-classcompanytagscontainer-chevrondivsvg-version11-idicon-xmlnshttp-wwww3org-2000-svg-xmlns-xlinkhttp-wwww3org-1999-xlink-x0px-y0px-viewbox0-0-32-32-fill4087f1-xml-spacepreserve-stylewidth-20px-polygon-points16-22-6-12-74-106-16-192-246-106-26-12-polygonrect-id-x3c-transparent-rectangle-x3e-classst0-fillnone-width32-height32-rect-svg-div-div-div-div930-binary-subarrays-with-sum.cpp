1549e8e843c314a80d8352f8667291d3a24f1597
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-03-14 11:50:07 +0530

    Time: 20 ms (84.40%), Space: 31.2 MB (52.67%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) 
+    
+    // total number of subarrays with a sum of k
+    int totalSubarraysAtmostK(vector<int>&nums, int k)
     {
         int ans = 0;
-        int prefixSum = 0;
-        unordered_map<int,int>freq; // prefixSum : frequency
+        int sum = 0;
+        int release = 0;
         
-        for(auto current : nums)
+        for(int current = 0; current < nums.size(); current++)
         {
-            prefixSum += current;
+            sum += nums[current];
             
-            if(prefixSum == goal)
-                ans++;
+            // invalid window
+            while(release <= current && sum > k)
+            {
+                sum -= nums[release];
+                release++;
+            }
             
-            if(freq[prefixSum-goal])
-                ans += freq[prefixSum-goal];
-            
-            freq[prefixSum]++;
+            ans += (current-release+1); 
         }
         return ans;
+    }
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return totalSubarraysAtmostK(nums,goal) - totalSubarraysAtmostK(nums,goal-1);
     }
 };
