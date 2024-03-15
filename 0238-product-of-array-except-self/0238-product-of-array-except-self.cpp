8bb967e288c7d0100fddfe4e7f9197bbb913c49a
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-03-15 11:37:01 +0530

    Time: 16 ms (59.62%), Space: 27.5 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,42 +3,19 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
         int n = nums.size();
-        int totalProduct = 1;
-        int zeroCount = 0;
-        int zeroIndex = 0;
-        vector<int>prefix(n,1);
-        vector<int>sufix(n,1);
         vector<int>ans(n,0);
+        vector<int>prefix(n,1);
+        vector<int>suffix(n,1);
         
-        //o(n)
-        for(int index = 0; index < n; index++)
-        {
-            if(nums[index] == 0)
-            {
-                zeroCount++;
-                zeroIndex = index;
-                continue;
-            }
-            totalProduct = (long long)totalProduct * nums[index];
-            
-            if(index == 0)
-                continue;
-                
-            prefix[index] = (long long)prefix[index-1]*nums[index-1];
-            sufix[n-index-1] = (long long)sufix[n-index]*nums[n-index];
-        }
+        for(int index = 1; index < n; index++)
+            prefix[index] = prefix[index-1] * nums[index-1];
         
-        if(zeroCount > 1)
-            return ans;
-        
-        if(zeroCount == 1)
-        {
-            ans[zeroIndex] = totalProduct;
-            return ans;
-        }
+        for(int index = n-2; index >= 0; index--)
+            suffix[index] = suffix[index+1]*nums[index+1];
         
         for(int index = 0; index < n; index++)
-            ans[index] = prefix[index] * sufix[index];
+            ans[index] = prefix[index] * suffix[index];
+        
         return ans;
     }
 };
